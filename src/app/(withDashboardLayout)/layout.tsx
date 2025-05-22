de513c877b219fe9a8de758a9a17db94e0c1de39
nafis200 <nafisahamed14@gmail.com>
2025-05-23 02:59:56 +0600

    pagination add

--- src/app/(withDashboardLayout)/layout.tsx
+++ src/app/(withDashboardLayout)/layout.tsx
@@ -12,6 +12,7 @@ export default function DashboardLayout({
     children: React.ReactNode;
   }) {
   return (
+    <>
     <SidebarProvider>
       <AppSidebar />
       <SidebarInset>
@@ -23,5 +24,6 @@ export default function DashboardLayout({
         <div className="p-4 pt-0 min-h-screen">{children}</div>
       </SidebarInset>
     </SidebarProvider>
+    </>
   )
 }
